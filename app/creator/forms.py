e8db7835f7615ae6eb77f1b644dee0230ef5df09
Orillion <orillion@localhost.localdomain>
2019-10-18 18:17:04 +0200

    Cretor html updated

--- app/creator/forms.py
+++ app/creator/forms.py
@@ -1,28 +1,29 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, BooleanField, SubmitField, RadioField, IntegerField, FloatField
+from wtforms import StringField, PasswordField, BooleanField, SubmitField, RadioField, IntegerField, FloatField, SelectField
 from wtforms.validators import ValidationError, DataRequired, Email, EqualTo
 from flask_babel import _, lazy_gettext as _l
 from app.models import User
 
 
 class Form_Creator(FlaskForm):
-    name = StringField(_l('Username'), validators=[DataRequired()])
-    sex = RadioField(_l('Sex'), choices=[('m','m'),('f','f'),('x','x')], validators=[DataRequired()])
-    job = StringField(_l('Job'), validators=[DataRequired()])
-    age = IntegerField(_l('age'), validators=[DataRequired()])
-    size = FloatField(_l('Size'), validators=[DataRequired()])
+    name = StringField(_l('Username'), validators=[DataRequired()],render_kw={"placeholder": "Charaktername"})
+    sex = RadioField(_l('Sex'), choices=[('m','männlich'),('f','weiblich'),('x','divers')], validators=[DataRequired()])
+    image = StringField(_l('Image'),render_kw={"placeholder": "Ein Characterbild"})
+    job = StringField(_l('Job'), validators=[DataRequired()],render_kw={"placeholder": "Beruf"})
+    age = IntegerField(_l('age'), validators=[DataRequired()],render_kw={"placeholder": "Alter"})
+    size = FloatField(_l('Size'), validators=[DataRequired()],render_kw={"placeholder": "Größe in cm"})
 
-    KK = IntegerField(_l('KK'), validators=[DataRequired()])
-    AU = IntegerField(_l('AU'), validators=[DataRequired()])
-    GE = IntegerField(_l('GE'), validators=[DataRequired()])
-    IN = IntegerField(_l('IN'), validators=[DataRequired()])
-    CH = IntegerField(_l('CH'), validators=[DataRequired()])
-    MB = IntegerField(_l('MB'), validators=[DataRequired()])
-    ATN = IntegerField(_l('ATN'), validators=[DataRequired()])
-    PA = IntegerField(_l('PA'), validators=[DataRequired()])
-    ATD = IntegerField(_l('ATD'), validators=[DataRequired()])
-    INI = IntegerField(_l('INI'), validators=[DataRequired()])
-    LE = IntegerField(_l('LE'), validators=[DataRequired()])
-    GG = IntegerField(_l('GG'), validators=[DataRequired()])
+    KK = IntegerField(_l('KK'), validators=[DataRequired()],render_kw={"placeholder": "Körperkraft"})
+    AU = IntegerField(_l('AU'), validators=[DataRequired()],render_kw={"placeholder": "Ausdauer"})
+    GE = IntegerField(_l('GE'), validators=[DataRequired()],render_kw={"placeholder": "Geschicklichkeit"})
+    IN = IntegerField(_l('IN'), validators=[DataRequired()],render_kw={"placeholder": "Intelligenz"})
+    CH = IntegerField(_l('CH'), validators=[DataRequired()],render_kw={"placeholder": "Charisma"})
+    MB = IntegerField(_l('MB'), validators=[DataRequired()],render_kw={"placeholder": "Mentale Belastbarkeit"})
+    ATN = IntegerField(_l('ATN'), validators=[DataRequired()],render_kw={"placeholder": "Attacke Nahkampf"})
+    PA = IntegerField(_l('PA'), validators=[DataRequired()],render_kw={"placeholder": "Parade"})
+    ATD = IntegerField(_l('ATD'), validators=[DataRequired()],render_kw={"placeholder": "Attacke Fernkampf"})
+    INI = IntegerField(_l('INI'), validators=[DataRequired()],render_kw={"placeholder": "Initiative"})
+    LE = IntegerField(_l('LE'), validators=[DataRequired()],render_kw={"placeholder": "Lebensenergie"})
+    GG = IntegerField(_l('GG'), validators=[DataRequired()],render_kw={"placeholder": "Geistige Gesundheit"})
 
     submit = SubmitField(_l('Sign In'))
